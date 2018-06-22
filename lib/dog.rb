61ee5a02fd7736cc7137422d9ea87bdf55e90939
abadug5793 <bavani7777@gmail.com>
2018-06-22 19:27:52 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # dog.rb
+class Dog
+
+  def name=(name)   #setter
+    @name = name
+  end
+  def name          #getter
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+
+
+
+end
